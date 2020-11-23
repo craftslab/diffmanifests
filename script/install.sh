47ef7c9a6cca4f781f9ab1b86e26771689ada328
Jia <49358172+craftslab@users.noreply.github.com>
2020-11-23 20:46:12 +0800

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -4,4 +4,4 @@ pip3 install -U pywin32
 pip3 install -U pyinstaller
 pip3 install -Ur requirements.txt
 
-pyinstaller --clean --name diffmanifests --upx-dir /path/to/upx -F diff.py
+pyinstaller --clean --name diffmanifests --upx-dir /usr/bin/upx -F diff.py
